dfe1bc6be242f3f017921e70afc71ea0dd3be822
Taylor Martin <taylormartin.uga@gmail.com>
2014-09-03 12:06:50 -0400

    fixed a DB column name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140903153951) do
+ActiveRecord::Schema.define(version: 20140903160445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20140903153951) do
     t.boolean  "bye",        default: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "NFL_team"
+    t.string   "nfl_team"
     t.string   "pos"
   end
 
